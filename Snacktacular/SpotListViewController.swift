c5f7e5e6eb93a69ba4a869f4e330fad267fdf536
Ryu Mondesir <mondesry@bc.edu>
2020-11-04 19:41:05 -0500

    8.4

--- Snacktacular/SpotListViewController.swift
+++ Snacktacular/SpotListViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class SpotListViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var sortSegmentedControl: UISegmentedControl!
     
     var spots = ["Island Creek Oysters", "El Pelon", "Shake Shack", "Pino's Pizza"]
     
@@ -16,10 +17,17 @@ class SpotListViewController: UIViewController {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-
+        configureSegmentedControl()
        
     }
-
+    func configureSegmentedControl() {
+        let orangeFontColor = [NSAttributedString.Key.foregroundColor : UIColor(named: "PrimaryColor") ?? UIColor.orange]
+        let whiteFontColor = [NSAttributedString.Key.foregroundColor : UIColor.white]
+        sortSegmentedControl.setTitleTextAttributes(orangeFontColor, for: .selected)
+        sortSegmentedControl.setTitleTextAttributes(whiteFontColor, for: .normal)
+        sortSegmentedControl.layer.borderColor = UIColor.white.cgColor
+        sortSegmentedControl.layer.borderWidth = 1.0
+    }
 }
 
 extension SpotListViewController: UITableViewDelegate, UITableViewDataSource {
